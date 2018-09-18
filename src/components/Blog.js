ed4b0e1121b5e7d972a104d835ce201f789ff7b6
pirjotuulia <pirjoleppanen@nic.fi>
2018-09-18 09:30:50 +0300

    5.14

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -23,7 +23,7 @@ class Blog extends React.Component {
     let blog = this.state.blog
     blog.likes += 1
     let updatedBlog = await blogService.update(blog.id, blog)
-    this.setState({blog: updatedBlog})
+    this.setState({ blog: updatedBlog })
   }
 
   handleDelete = (event) => {
@@ -34,15 +34,18 @@ class Blog extends React.Component {
   }
 
   render() {
-    const user = JSON.parse(window.localStorage.getItem('user'))
+    let user = null
+    if (window.localStorage) {
+      user = JSON.parse(window.localStorage.getItem('user'))
+    }
     return (
       <div className="blogentry" >
-        <div onClick={this.toggleDetails}>{this.state.blog.title} {this.state.blog.author}</div>
-        {this.state.visible && <div>
+        <div className='namediv' onClick={this.toggleDetails}>{this.state.blog.title} {this.state.blog.author}</div>
+        {this.state.visible && <div className='infodiv'>
           <a href={this.state.blog.url}>{this.state.blog.url}</a><br></br>
           {this.state.blog.likes} likes <button onClick={this.addLike}>likes</button><br></br>
           added by {this.state.blog.author}<br></br>
-          {(!this.state.blog.user||this.state.blog.user.username===user.username)&&<button id={this.state.blog.id} onClick={this.handleDelete}>delete</button>}
+          {(!this.state.blog.user || this.state.blog.user.username === user.username) && <button id={this.state.blog.id} onClick={this.handleDelete}>delete</button>}
         </div>}
       </div>
     )
---
+++ src/components/Blog.test.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Blog from './Blog'
+
+describe.only('<Blog />', () => {
+    it('renders content', () => {
+        const blog = {
+            title: 'Testing testing',
+            author: 'Tester',
+            url: 'http://test.com',
+            likes: 6
+        }
+
+        const blogComponent = shallow(<Blog blog={blog} />)
+        const infoDiv = blogComponent.find('.blogentry')
+        // console.log(blogComponent.debug())
+        // console.log(infoDiv.debug())
+
+        expect(infoDiv.text()).toContain(blog.title)
+        expect(infoDiv.text()).toContain(blog.author)
+    })
+    it('after clicking name the details are displayed', () => {
+        const blog = {
+            title: 'Testing testing',
+            author: 'Tester',
+            url: 'http://test.com',
+            likes: 6
+        }
+        const mockHandler = jest.fn()
+        const blogComponent = shallow(<Blog blog={blog} onClick={mockHandler} />)
+
+        const nameDiv = blogComponent.find('.namediv')
+        // console.log(nameDiv.debug())
+        nameDiv.simulate('click')
+
+        const infoDiv = blogComponent.find('.infodiv')
+        // console.log(infoDiv.debug())
+        expect(infoDiv.text()).toContain(blog.url)
+        expect(infoDiv.text()).toContain(blog.likes.toString())
+    })
+})
+
+

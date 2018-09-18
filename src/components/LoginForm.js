611665e08bd8db60dc98ca4cd4b1c0897ee40540
pirjotuulia <pirjoleppanen@nic.fi>
2018-09-18 08:20:37 +0300

    5.11

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const loginForm = ({ handleSubmit, handleChange, username, password }) => (
     <div>
@@ -26,5 +27,12 @@ const loginForm = ({ handleSubmit, handleChange, username, password }) => (
         </form>
     </div>
 )
+
+LoginForm.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+  }
 
 export default loginForm
